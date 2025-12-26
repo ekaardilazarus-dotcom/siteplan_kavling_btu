53b83f8064e275d4d879613182964a792fb7a590
ekaardilazarus-dotcom <ekaardilazarus@gmail.com>
2025-12-26 11:43:50 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-fetch('sitemap.svg')
-  .then(res => res.text())
-  .then(svg => {
-    document.getElementById('map').innerHTML = svg;
-  })
-  .catch(err => console.error('Gagal load SVG:', err));
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('./sitemap.svg')
+    .then(res => res.text())
+    .then(svg => {
+      document.getElementById('map').innerHTML = svg;
+      console.log('SVG INLINE BERHASIL');
+    })
+    .catch(err => console.error('Gagal load SVG:', err));
+});
