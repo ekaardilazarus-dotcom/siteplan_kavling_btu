588c4af9a15e40cef7b6b44d29aeee56cd8656c1
ekaardilazarus-dotcom <ekaardilazarus@gmail.com>
2025-12-26 11:36:31 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("script.js aktif");
+fetch('sitemap.svg')
+  .then(res => res.text())
+  .then(svg => {
+    document.getElementById('map').innerHTML = svg;
+  })
+  .catch(err => console.error('Gagal load SVG:', err));
